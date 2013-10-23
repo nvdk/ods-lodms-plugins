ddbf2402c0dae57890f2dd9938842d418afe1e2b
nielsv <niels.vandekeybus@tenforce.com>
2013-10-23 12:25:36 +0200

    expanded ODSVoc with more DCAT vocabulary

--- general-libs/ODSVoc/src/main/java/com/tenforce/lodms/ODSVoc.java
+++ general-libs/ODSVoc/src/main/java/com/tenforce/lodms/ODSVoc.java
@@ -13,6 +13,8 @@ public interface ODSVoc {
     URI DCAT_CAT_PROP_RECORD = valueFactory.createURI(DCAT + "#record");
     URI DCAT_CATALOGRECORD = valueFactory.createURI(DCAT + "#CatalogRecord");
     URI DCAT_DATASET = valueFactory.createURI(DCAT + "#Dataset");
+    URI DCAT_MEDIA_TYPE = valueFactory.createURI(DCAT + "mediaType");
+    URI DCAT_THEME = valueFactory.createURI(DCAT + "#theme");
     URI FOAF_PRIMARYTOPIC = valueFactory.createURI("http://xmlns.com/foaf/0.1/primaryTopic");
     String DC_TERMS = "http://purl.org/dc/terms/";
     URI DCT_PUBLISHER = valueFactory.createURI(DC_TERMS + "publisher");
@@ -20,10 +22,18 @@ public interface ODSVoc {
     URI DCT_DESCRIPTION = valueFactory.createURI(DC_TERMS + "description");
     URI DCT_TITLE = valueFactory.createURI(DC_TERMS + "title");
     URI DCT_LICENSE = valueFactory.createURI(DC_TERMS + "license");
-    URI ADMS_STATUS = valueFactory.createURI("http://www.w3.org/ns/adms#status");
+    URI DCT_FORMAT = valueFactory.createURI(DC_TERMS + "format");
+    URI DCT_SPATIAL = valueFactory.createURI(DC_TERMS + "spatial");
+    URI DCT_LANGUAGE = valueFactory.createURI(DC_TERMS + "language");
+    URI DCT_ACCRUAL_PERIODICTY = valueFactory.createURI(DC_TERMS + "accrualPeriodicity");
+    URI DCT_TYPE = valueFactory.createURI(DC_TERMS + "type");
+    String ADMS_NS = "http://www.w3.org/ns/adms#";
+    URI ADMS_STATUS = valueFactory.createURI(ADMS_NS + "status");
+    URI ADMS_CONTACT_POINT = valueFactory.createURI(ADMS_NS + "contactPoint");
     URI ODS_NS = valueFactory.createURI("http://data.opendatasupport.eu/ontology/harmonisation.owl#");
     URI ODS_RAW_CATALOG = valueFactory.createURI(ODS_NS + "raw_catalog");
     URI ODS_RAW_DATASET = valueFactory.createURI(ODS_NS + "raw_dataset");
     URI ODS_HARVEST_DATE = valueFactory.createURI(ODS_NS + "harvest_date");
+
 
 }
