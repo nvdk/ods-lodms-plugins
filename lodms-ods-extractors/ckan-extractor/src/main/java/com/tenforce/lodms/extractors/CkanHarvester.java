e0d4040b798ee82432637523daee4406c83a6c64
nielsv <niels.vandekeybus@tenforce.com>
2013-11-12 11:38:30 +0100

    memory optimization in ckan harvester

--- lodms-ods-extractors/ckan-extractor/src/main/java/com/tenforce/lodms/extractors/CkanHarvester.java
+++ lodms-ods-extractors/ckan-extractor/src/main/java/com/tenforce/lodms/extractors/CkanHarvester.java
@@ -9,7 +9,6 @@ import org.openrdf.model.impl.StatementImpl;
 import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.rio.RDFHandler;
 import org.openrdf.rio.RDFHandlerException;
-import org.springframework.web.client.RestTemplate;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
@@ -25,127 +24,125 @@ import java.util.concurrent.Future;
 
 
 public class CkanHarvester {
-    private static final ValueFactory valueFactory = ValueFactoryImpl.getInstance();
+  private static final ValueFactory valueFactory = ValueFactoryImpl.getInstance();
 
-    private String subjectPrefix;
-    private String predicatePrefix;
-    private String apiUri;
-    private String baseUri;
-    private RestTemplate rest;
-    private List<String> ignoredKeys;
-    private RDFHandler handler;
-    private boolean enableProvenance = true;
-    private String catalogOwner = "";
-    private List<String> warnings = new ArrayList<String>();
-    private String catalogTitle;
-    private String catalogDescription;
-    private String license;
+  private String subjectPrefix;
+  private String predicatePrefix;
+  private String apiUri;
+  private String baseUri;
+  private List<String> ignoredKeys;
+  private RDFHandler handler;
+  private boolean enableProvenance = true;
+  private String catalogOwner = "";
+  private List<String> warnings = new ArrayList<String>();
+  private String catalogTitle;
+  private String catalogDescription;
+  private String license;
 
-    public CkanHarvester(String baseUri, String subjectPrefix, String predicatePrefix, RDFHandler handler) {
-        this.subjectPrefix = subjectPrefix;
-        this.predicatePrefix = predicatePrefix;
-        this.handler = handler;
-        this.baseUri = baseUri;
-        apiUri = baseUri.endsWith("/") ? baseUri + "api/3/" : baseUri + "/api/3/";
-        rest = RestTemplateFactory.getRestTemplate();
+  public CkanHarvester(String baseUri, String subjectPrefix, String predicatePrefix, RDFHandler handler) {
+    this.subjectPrefix = subjectPrefix;
+    this.predicatePrefix = predicatePrefix;
+    this.handler = handler;
+    this.baseUri = baseUri;
+    apiUri = baseUri.endsWith("/") ? baseUri + "api/3/" : baseUri + "/api/3/";
+  }
+
+  public void setIgnoredKeys(List<String> ignoredKeys) {
+    this.ignoredKeys = ignoredKeys;
+  }
+
+  private void addCatalogProvenance() throws RDFHandlerException, DatatypeConfigurationException {
+    URI source = valueFactory.createURI(baseUri);
+    handler.handleStatement(new StatementImpl(source, ODSVoc.ODS_HARVEST_DATE, valueFactory.createLiteral(getXMLNow())));
+    handler.handleStatement(new StatementImpl(source, ODSVoc.RDFTYPE, ODSVoc.DCAT_CATALOG));
+
+    if (!catalogOwner.isEmpty())
+      handler.handleStatement(new StatementImpl(source, ODSVoc.DCT_PUBLISHER, valueFactory.createLiteral(catalogOwner)));
+    if (catalogDescription != null && !catalogDescription.isEmpty())
+      handler.handleStatement(new StatementImpl(source, ODSVoc.DCT_DESCRIPTION, valueFactory.createLiteral(catalogDescription)));
+    if (catalogTitle != null && !catalogTitle.isEmpty())
+      handler.handleStatement(new StatementImpl(valueFactory.createURI(baseUri), ODSVoc.DCT_TITLE, valueFactory.createLiteral(catalogTitle)));
+    if (license != null && !license.isEmpty())
+      handler.handleStatement(new StatementImpl(valueFactory.createURI(baseUri), ODSVoc.DCT_LICENSE, valueFactory.createLiteral(license)));
+  }
+
+  private void addDataSetProvenance(String datasetId) throws RDFHandlerException, DatatypeConfigurationException {
+    URI source = valueFactory.createURI(baseUri);
+    URI dataset = valueFactory.createURI(subjectPrefix + datasetId);
+    handler.handleStatement(new StatementImpl(source, ODSVoc.DCAT_CAT_PROP_DATASET, valueFactory.createURI(subjectPrefix + datasetId)));
+    handler.handleStatement(new StatementImpl(dataset, ODSVoc.RDFTYPE, ODSVoc.DCAT_DATASET));
+    handler.handleStatement(new StatementImpl(dataset, ODSVoc.ODS_HARVEST_DATE, valueFactory.createLiteral(getXMLNow())));
+  }
+
+  public void harvest() throws RDFHandlerException, ExtractException, DatatypeConfigurationException {
+    List<String> datasetIds = CkanDataSetList.getPackageIds(apiUri);
+    if (datasetIds.isEmpty())
+      throw new ExtractException("no datasets found in packageList: " + apiUri);
+
+    harvest(datasetIds);
+  }
+
+  public void harvest(List<String> datasetIds) throws RDFHandlerException, ExtractException, DatatypeConfigurationException {
+    if (datasetIds.isEmpty()) {
+      throw new ExtractException("no datasets specified");
     }
+    if (enableProvenance)
+      addCatalogProvenance();
 
-    public void setIgnoredKeys(List<String> ignoredKeys) {
-        this.ignoredKeys = ignoredKeys;
+    ExecutorService executorService = Executors.newFixedThreadPool(5);
+    Collection<Future<List<Statement>>> futures = new ArrayList<Future<List<Statement>>>();
+
+    for (String datasetId : datasetIds) {
+      addDataSetProvenance(datasetId);
+      MapToRdfConverter converter = new MapToRdfConverter(subjectPrefix + datasetId, predicatePrefix, ignoredKeys);
+      converter.setSubjectPrefix(subjectPrefix + datasetId);
+      DataSetHarvester harvester = new DataSetHarvester(converter, apiUri, datasetId);
+      futures.add(executorService.submit(harvester));
     }
-
-    private void addCatalogProvenance() throws RDFHandlerException, DatatypeConfigurationException {
-        URI source = valueFactory.createURI(baseUri);
-        handler.handleStatement(new StatementImpl(source, ODSVoc.ODS_HARVEST_DATE, valueFactory.createLiteral(getXMLNow())));
-        handler.handleStatement(new StatementImpl(source, ODSVoc.RDFTYPE, ODSVoc.DCAT_CATALOG));
-
-        if (!catalogOwner.isEmpty())
-            handler.handleStatement(new StatementImpl(source, ODSVoc.DCT_PUBLISHER, valueFactory.createLiteral(catalogOwner)));
-        if (catalogDescription != null && !catalogDescription.isEmpty())
-            handler.handleStatement(new StatementImpl(source, ODSVoc.DCT_DESCRIPTION, valueFactory.createLiteral(catalogDescription)));
-        if (catalogTitle != null && !catalogTitle.isEmpty())
-            handler.handleStatement(new StatementImpl(valueFactory.createURI(baseUri), ODSVoc.DCT_TITLE, valueFactory.createLiteral(catalogTitle)));
-        if (license != null && !license.isEmpty())
-            handler.handleStatement(new StatementImpl(valueFactory.createURI(baseUri), ODSVoc.DCT_LICENSE, valueFactory.createLiteral(license)));
-    }
-
-    private void addDataSetProvenance(String datasetId) throws RDFHandlerException, DatatypeConfigurationException {
-        URI source = valueFactory.createURI(baseUri);
-        URI dataset = valueFactory.createURI(subjectPrefix + datasetId);
-        handler.handleStatement(new StatementImpl(source, ODSVoc.DCAT_CAT_PROP_DATASET, valueFactory.createURI(subjectPrefix + datasetId)));
-        handler.handleStatement(new StatementImpl(dataset, ODSVoc.RDFTYPE, ODSVoc.DCAT_DATASET));
-        handler.handleStatement(new StatementImpl(dataset, ODSVoc.ODS_HARVEST_DATE, valueFactory.createLiteral(getXMLNow())));
-    }
-
-    public void harvest() throws RDFHandlerException, ExtractException, DatatypeConfigurationException {
-        List<String> datasetIds = CkanDataSetList.getPackageIds(apiUri);
-        if (datasetIds.isEmpty())
-            throw new ExtractException("no datasets found in packageList: " + apiUri);
-
-        harvest(datasetIds);
-    }
-
-    public void harvest(List<String> datasetIds) throws RDFHandlerException, ExtractException, DatatypeConfigurationException {
-        if (datasetIds.isEmpty()) {
-            throw new ExtractException("no datasets specified");
+    for (Future<List<Statement>> future : futures) {
+      try {
+        List<Statement> statements = future.get();
+        for (Statement statement : statements) {
+          handler.handleStatement(statement);
         }
-        if (enableProvenance)
-            addCatalogProvenance();
-
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        Collection<Future<List<Statement>>> futures = new ArrayList<Future<List<Statement>>>();
-
-        for (String datasetId : datasetIds) {
-            addDataSetProvenance(datasetId);
-            MapToRdfConverter converter = new MapToRdfConverter(subjectPrefix + datasetId, predicatePrefix, ignoredKeys);
-            converter.setSubjectPrefix(subjectPrefix + datasetId);
-            DataSetHarvester harvester = new DataSetHarvester(converter, apiUri, datasetId);
-            futures.add(executorService.submit(harvester));
-        }
-        for (Future<List<Statement>> future : futures) {
-            try {
-                List<Statement> statements = future.get();
-                for (Statement statement : statements) {
-                    handler.handleStatement(statement);
-                }
-            } catch (ExecutionException e) {
-                warnings.add(e.getMessage());
-            } catch (Exception e) {
-                executorService.shutdownNow();
-                throw new ExtractException(e.getMessage(), e);
-            }
-        }
-        executorService.shutdown();
+      } catch (ExecutionException e) {
+        warnings.add(e.getMessage());
+      } catch (Exception e) {
+        executorService.shutdownNow();
+        throw new ExtractException(e.getMessage(), e);
+      }
     }
+    executorService.shutdown();
+  }
 
-    private static XMLGregorianCalendar getXMLNow() throws DatatypeConfigurationException {
-        GregorianCalendar gregorianCalendar = new GregorianCalendar();
-        DatatypeFactory datatypeFactory = null;
-        datatypeFactory = DatatypeFactory.newInstance();
-        return datatypeFactory.newXMLGregorianCalendar(gregorianCalendar);
-    }
+  private static XMLGregorianCalendar getXMLNow() throws DatatypeConfigurationException {
+    GregorianCalendar gregorianCalendar = new GregorianCalendar();
+    DatatypeFactory datatypeFactory = null;
+    datatypeFactory = DatatypeFactory.newInstance();
+    return datatypeFactory.newXMLGregorianCalendar(gregorianCalendar);
+  }
 
-    public void setEnableProvenance(boolean enableProvenance) {
-        this.enableProvenance = enableProvenance;
-    }
+  public void setEnableProvenance(boolean enableProvenance) {
+    this.enableProvenance = enableProvenance;
+  }
 
-    public void setCatalogOwner(String catalogOwner) {
-        this.catalogOwner = catalogOwner;
-    }
+  public void setCatalogOwner(String catalogOwner) {
+    this.catalogOwner = catalogOwner;
+  }
 
-    public void setWarnings(List<String> warnings) {
-        this.warnings = warnings;
-    }
+  public void setWarnings(List<String> warnings) {
+    this.warnings = warnings;
+  }
 
-    public void setCatalogTitle(String catalogTitle) {
-        this.catalogTitle = catalogTitle;
-    }
+  public void setCatalogTitle(String catalogTitle) {
+    this.catalogTitle = catalogTitle;
+  }
 
-    public void setCatalogDescription(String catalogDescription) {
-        this.catalogDescription = catalogDescription;
-    }
+  public void setCatalogDescription(String catalogDescription) {
+    this.catalogDescription = catalogDescription;
+  }
 
-    public void setLicense(String license) {
-        this.license = license;
-    }
+  public void setLicense(String license) {
+    this.license = license;
+  }
 }
--- lodms-ods-extractors/ckan-extractor/src/main/java/com/tenforce/lodms/extractors/MapToRdfConverter.java
+++ lodms-ods-extractors/ckan-extractor/src/main/java/com/tenforce/lodms/extractors/MapToRdfConverter.java
@@ -10,143 +10,145 @@ import java.util.*;
 import java.util.concurrent.Callable;
 
 public class MapToRdfConverter implements Callable<List<Statement>> {
-    private Map<String, Object> map;
-    private String predicatePrefix = "http://odp.tenforce.com/statement";
-    private final List<Statement> statements = new ArrayList<Statement>();
-    private final ValueFactory valueFactory = ValueFactoryImpl.getInstance();
-    private List<String> ignoredKeys = new ArrayList<String>();
-    private String subjectPrefix;
-    protected Logger logger = org.apache.log4j.Logger.getLogger(MapToRdfConverter.class);
+  private Map<String, Object> map;
+  private String predicatePrefix = "http://odp.tenforce.com/statement";
+  private final List<Statement> statements = new ArrayList<Statement>();
+  private final ValueFactory valueFactory = ValueFactoryImpl.getInstance();
+  private List<String> ignoredKeys = new ArrayList<String>();
+  private String subjectPrefix;
+  protected Logger logger = org.apache.log4j.Logger.getLogger(MapToRdfConverter.class);
 
-    public MapToRdfConverter(String subjectPrefix, String predicatePrefix, List<String> ignoredKeys) {
-        this.subjectPrefix = subjectPrefix;
-        this.predicatePrefix = predicatePrefix;
-        this.ignoredKeys = ignoredKeys;
+  public MapToRdfConverter(String subjectPrefix, String predicatePrefix, List<String> ignoredKeys) {
+    this.subjectPrefix = subjectPrefix;
+    this.predicatePrefix = predicatePrefix;
+    this.ignoredKeys = ignoredKeys;
+  }
+
+  public void setSubjectPrefix(String subjectPrefix) {
+    this.subjectPrefix = subjectPrefix;
+  }
+
+  public void setMap(Map<String, Object> map) {
+    this.map = map;
+  }
+
+  public List<Statement> convert() {
+    String provenancePrefix = "http://data.opendatasupport.eu/ontology/harmonisation.owl#";
+    URI contentHash = valueFactory.createURI(provenancePrefix + "content_hash");
+    Statement s = valueFactory.createStatement(valueFactory.createURI(subjectPrefix), contentHash, valueFactory.createLiteral(map.hashCode()));
+    statements.add(s);
+    convertHashmapToStatements(subjectPrefix, map);
+    List<Statement> result = Collections.unmodifiableList(statements);
+    statements.clear();
+    return result;
+  }
+
+  /*
+  * Convert a list to a set of statements
+  * @param String subject
+  * @param String key
+  * @param List  list
+  */
+  private void convertListToStatements(String subjectStr, String key, List list) {
+    // NOTE: currently doesn't support nested list
+    // NOTE: this will not store empty arrays
+    URI subject = valueFactory.createURI(subjectStr);
+    URI predicate = valueFactory.createURI(generateKey(predicatePrefix, key));
+
+    Integer i = 0;
+    for (Object o : list) {
+      String newSubj = generateKey(generateKey(subjectStr, key), i.toString());
+      if (o instanceof String) {
+        storeValue(subject, predicate, (String) o);
+      } else if (o instanceof Integer) {
+        storeValue(subject, predicate, (Integer) o);
+      } else if (o instanceof Boolean) {
+        storeValue(subject, predicate, (Boolean) o);
+      } else if (o instanceof HashMap) {
+        statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createURI(newSubj)));
+        convertHashmapToStatements(newSubj, (Map<String, Object>) o);
+      }
+      i++;
     }
+  }
 
-    public void setSubjectPrefix(String subjectPrefix) {
-        this.subjectPrefix = subjectPrefix;
+  private void convertHashmapToStatements(String subject, Map<String, Object> map) {
+    for (Map.Entry<String, Object> pairs : map.entrySet()) {
+      convertPairToStatements(subject, pairs.getKey(), pairs.getValue());
     }
+  }
 
-    public void setMap(Map<String, Object> map) {
-        this.map = map;
+  /*
+  * Convert a key value pair to set of statements
+  * @params String subject
+  * @param String key
+  * @param Object value
+  */
+  private void convertPairToStatements(String subjectStr, String key, Object value) {
+    URI subject = valueFactory.createURI(subjectStr);
+    URI predicate = valueFactory.createURI(predicatePrefix, key);
+
+    // don't do anything for ignored keys
+    if (ignoredKeys.contains(key))
+      return;
+
+    // NOTE: not storing null values
+    if (value == null)
+      return;
+
+
+    if (value instanceof String) {
+      String v = (String) value;
+      if (!v.isEmpty())
+        storeValue(subject, predicate, v);
+    } else if (value instanceof Boolean) {
+      storeValue(subject, predicate, (Boolean) value);
+    } else if (value instanceof Integer) {
+      storeValue(subject, predicate, (Integer) value);
+    } else if (value instanceof Map.Entry) {
+      Map.Entry<String, Object> keyValuePair = (Map.Entry<String, Object>) value;
+      String newSubj = generateKey(subjectStr, key);
+      statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createURI(newSubj)));
+      convertPairToStatements(newSubj, keyValuePair.getKey(), keyValuePair.getValue());
+    } else if (value instanceof List) {
+      convertListToStatements(subjectStr, key, (List) value);
+    } else if (value instanceof HashMap) {
+      convertHashmapToStatements(generateKey(subjectStr, key), (Map) value);
     }
-
-    public List<Statement> convert() {
-        String provenancePrefix = "http://data.opendatasupport.eu/ontology/harmonisation.owl#";
-        URI contentHash = valueFactory.createURI(provenancePrefix + "content_hash");
-        Statement s = valueFactory.createStatement(valueFactory.createURI(subjectPrefix), contentHash, valueFactory.createLiteral(map.hashCode()));
-        statements.add(s);
-        convertHashmapToStatements(subjectPrefix, map);
-        return Collections.unmodifiableList(statements);
+    // oops, encountered a value format we do not support yet
+    else {
+      logger.warn("unsupported class: " + value.getClass() + " for value " + value.toString());
     }
+  }
 
-    /*
-    * Convert a list to a set of statements
-    * @param String subject
-    * @param String key
-    * @param List  list
-    */
-    private void convertListToStatements(String subjectStr, String key, List list) {
-        // NOTE: currently doesn't support nested list
-        // NOTE: this will not store empty arrays
-        URI subject = valueFactory.createURI(subjectStr);
-        URI predicate = valueFactory.createURI(generateKey(predicatePrefix, key));
+  private void storeValue(URI subject, URI predicate, Integer value) {
+    statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createLiteral(value)));
+  }
 
-        Integer i = 0;
-        for (Object o : list) {
-            String newSubj = generateKey(generateKey(subjectStr, key), i.toString());
-            if (o instanceof String) {
-                storeValue(subject, predicate, (String) o);
-            } else if (o instanceof Integer) {
-                storeValue(subject, predicate, (Integer) o);
-            } else if (o instanceof Boolean) {
-                storeValue(subject, predicate, (Boolean) o);
-            } else if (o instanceof HashMap) {
-                statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createURI(newSubj)));
-                convertHashmapToStatements(newSubj, (Map<String, Object>) o);
-            }
-            i++;
-        }
-    }
+  private void storeValue(URI subject, URI predicate, String value) {
+    statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createLiteral(value)));
 
-    private void convertHashmapToStatements(String subject, Map<String, Object> map) {
-        for (Map.Entry<String, Object> pairs : map.entrySet()) {
-            convertPairToStatements(subject, pairs.getKey(), pairs.getValue());
-        }
-    }
+  }
 
-    /*
-    * Convert a key value pair to set of statements
-    * @params String subject
-    * @param String key
-    * @param Object value
-    */
-    private void convertPairToStatements(String subjectStr, String key, Object value) {
-        URI subject = valueFactory.createURI(subjectStr);
-        URI predicate = valueFactory.createURI(predicatePrefix, key);
+  private void storeValue(URI subject, URI predicate, Boolean value) {
+    statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createLiteral(value)));
+  }
 
-        // don't do anything for ignored keys
-        if (ignoredKeys.contains(key))
-            return;
+  /*
+  * Generate a unique identifier for this attribute in rdf
+  * @param String parentKey
+  * @param String subKey
+  *
+  * @return String attributeURI
+  */
+  private static String generateKey(String parentKey, String subKey) {
+    if (parentKey.isEmpty())
+      return subKey;
+    return parentKey.endsWith("/") ? parentKey + subKey : parentKey + '/' + subKey;
+  }
 
-        // NOTE: not storing null values
-        if (value == null)
-            return;
-
-
-        if (value instanceof String) {
-            String v = (String) value;
-            if (!v.isEmpty())
-                storeValue(subject, predicate, v);
-        } else if (value instanceof Boolean) {
-            storeValue(subject, predicate, (Boolean) value);
-        } else if (value instanceof Integer) {
-            storeValue(subject, predicate, (Integer) value);
-        } else if (value instanceof Map.Entry) {
-            Map.Entry<String, Object> keyValuePair = (Map.Entry<String, Object>) value;
-            String newSubj = generateKey(subjectStr, key);
-            statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createURI(newSubj)));
-            convertPairToStatements(newSubj, keyValuePair.getKey(), keyValuePair.getValue());
-        } else if (value instanceof List) {
-            convertListToStatements(subjectStr, key, (List) value);
-        } else if (value instanceof HashMap) {
-            convertHashmapToStatements(generateKey(subjectStr, key), (Map) value);
-        }
-        // oops, encountered a value format we do not support yet
-        else {
-            logger.warn("unsupported class: " + value.getClass() + " for value " + value.toString());
-        }
-    }
-
-    private void storeValue(URI subject, URI predicate, Integer value) {
-        statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createLiteral(value)));
-    }
-
-    private void storeValue(URI subject, URI predicate, String value) {
-        statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createLiteral(value)));
-
-    }
-
-    private void storeValue(URI subject, URI predicate, Boolean value) {
-        statements.add(valueFactory.createStatement(subject, predicate, valueFactory.createLiteral(value)));
-    }
-
-    /*
-    * Generate a unique identifier for this attribute in rdf
-    * @param String parentKey
-    * @param String subKey
-    *
-    * @return String attributeURI
-    */
-    private static String generateKey(String parentKey, String subKey) {
-        if (parentKey.isEmpty())
-            return subKey;
-        return parentKey.endsWith("/") ? parentKey + subKey : parentKey + '/' + subKey;
-    }
-
-    @Override
-    public List<Statement> call() throws Exception {
-        return convert();
-    }
+  @Override
+  public List<Statement> call() throws Exception {
+    return convert();
+  }
 }
