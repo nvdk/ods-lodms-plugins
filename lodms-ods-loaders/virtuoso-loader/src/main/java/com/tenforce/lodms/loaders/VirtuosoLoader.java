46506bcce551671cd2fc50c3af6aeb3994bfc795
nielsv <niels.vandekeybus@tenforce.com>
2013-12-04 16:48:08 +0100

    virtuoso loader: use virtuoso connection from core

--- lodms-ods-loaders/virtuoso-loader/src/main/java/com/tenforce/lodms/loaders/VirtuosoLoader.java
+++ lodms-ods-loaders/virtuoso-loader/src/main/java/com/tenforce/lodms/loaders/VirtuosoLoader.java
@@ -8,23 +8,18 @@ import at.punkt.lodms.integration.UIComponent;
 import at.punkt.lodms.spi.load.LoadContext;
 import at.punkt.lodms.spi.load.LoadException;
 import at.punkt.lodms.spi.load.Loader;
-import at.punkt.lodms.util.BatchedRdfInserter;
 import com.vaadin.Application;
 import com.vaadin.terminal.ClassResource;
 import com.vaadin.terminal.Resource;
 import org.apache.log4j.Logger;
-import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
-import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.impl.URIImpl;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.RepositoryResult;
-import org.openrdf.repository.util.RDFInserter;
-import org.openrdf.rio.RDFHandlerException;
 import virtuoso.sesame2.driver.VirtuosoRepository;
 
 public class VirtuosoLoader extends ConfigurableBase<VirtuosoLoaderConfig> implements Loader, UIComponent, ConfigDialogProvider<VirtuosoLoaderConfig> {
@@ -43,50 +38,30 @@ public class VirtuosoLoader extends ConfigurableBase<VirtuosoLoaderConfig> imple
         }
 
         try {
-            URI graphUri = rpstr.getValueFactory().createURI(graph);
-            if (versioned) {
-                backupGraph(graph);
+            RepositoryConnection connection = rpstr.getConnection();
+            try {
+                URI destinationGraph = new URIImpl(graph);
+                if (versioned)
+                    copyGraph(connection,destinationGraph,getBackupGraph(destinationGraph),false);
+                copyGraph(connection,uri,destinationGraph,true);
             }
-            RepositoryResult<Statement> statements = rpstr.getConnection().getStatements(null, null, null, true, uri);
-            loadwithInserter(statements, graphUri);
-            statements.close();
+            finally {
+                connection.close();
+            }
+
         } catch (RepositoryException e) {
             throw new LoadException(e.getMessage(), e);
         } catch (IllegalArgumentException e) {
             throw new LoadException(e.getMessage(), e);
-        } catch (RDFHandlerException e) {
-            lc.getWarnings().add(e.getMessage());
         }
     }
 
-    private void loadwithInserter(RepositoryResult<Statement> statements, URI graphUri) throws RepositoryException, RDFHandlerException {
-        RepositoryConnection virtuosoConnection = repository.getConnection();
-        virtuosoConnection.clear(graphUri);
-        virtuosoConnection.setAutoCommit(false);
+    private URI getBackupGraph(URI graph) throws LoadException {
+        return new URIImpl(graph.stringValue() + "previous");
+    }
+
+    private void copyGraph(RepositoryConnection connection,URI orgGraph, URI destGraph,boolean useCopy) throws LoadException {
         try {
-            RDFInserter inserter = new BatchedRdfInserter(virtuosoConnection, BATCH_SIZE);
-            inserter.enforceContext(graphUri);
-            while (statements.hasNext()) {
-                Statement s = statements.next();
-                inserter.handleStatement(s);
-            }
-            virtuosoConnection.commit();
-        }
-        finally {
-            virtuosoConnection.close();
-        }
-    }
-
-    private void backupGraph(String graph) throws LoadException {
-        URI backupGraph = ValueFactoryImpl.getInstance().createURI(graph + "previous");
-        URI orgGraph = ValueFactoryImpl.getInstance().createURI(graph);
-        copyGraph(repository,orgGraph, backupGraph,false);
-    }
-
-    private void copyGraph(Repository repository,URI orgGraph, URI destGraph,boolean useCopy) throws LoadException {
-        RepositoryConnection connection;
-        try {
-            connection = repository.getConnection();
             String action = useCopy ? "COPY" : "MOVE";
             String query = "define sql:log-enable 3 " + action +" <" + orgGraph + "> TO <" + destGraph + ">";
             connection.prepareGraphQuery(QueryLanguage.SPARQL, query).evaluate();
@@ -123,16 +98,7 @@ public class VirtuosoLoader extends ConfigurableBase<VirtuosoLoaderConfig> imple
 
     @Override
     protected void configureInternal(VirtuosoLoaderConfig config) throws ConfigurationException {
-        String connectionString = "jdbc:virtuoso://" + config.getHost() + ':' + config.getPort();
-        versioned = config.isVersioned();
-        repository = new VirtuosoRepository(connectionString, config.getUserName(), config.getPassword(), true);
-        logger.info("starting load");
-        try {
-            RepositoryConnection con = repository.getConnection();
-            con.close();
-        } catch (RepositoryException e) {
-            logger.error(e.getMessage());
-        }
+
     }
 
     @Override
--- lodms-ods-loaders/virtuoso-loader/src/main/java/com/tenforce/lodms/loaders/VirtuosoLoaderConfig.java
+++ lodms-ods-loaders/virtuoso-loader/src/main/java/com/tenforce/lodms/loaders/VirtuosoLoaderConfig.java
@@ -4,45 +4,9 @@ public class VirtuosoLoaderConfig {
     public static final String GRAPHSOURCE_CUSTOM = "custom";
     public static final String GRAPHSOURCE_CKANURI = "ckan uri";
     public static final String GRAPHSOURCE_ODSURI = "harmonized uri";
-    private String host = "localhost";
-    private String port = "1111";
-    private String userName = "dba";
-    private String password = "dba";
     private boolean versioned = false;
     private String graphSource = GRAPHSOURCE_CUSTOM;
     private String graph = "";
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
 
     public boolean isVersioned() {
         return versioned;
