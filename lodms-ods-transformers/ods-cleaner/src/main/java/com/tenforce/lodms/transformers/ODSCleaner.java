d2c38736cada603b613a6521321678211b4bcb7f
nielsv <niels.vandekeybus@tenforce.com>
2014-01-27 15:02:38 +0100

    ods cleaner: (optimization) use property paths to delete all triples connected to the raw catalog

--- lodms-ods-transformers/ods-cleaner/src/main/java/com/tenforce/lodms/transformers/ODSCleaner.java
+++ lodms-ods-transformers/ods-cleaner/src/main/java/com/tenforce/lodms/transformers/ODSCleaner.java
@@ -16,54 +16,60 @@ import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 
 public class ODSCleaner extends TransformerBase<Object> {
-    @Override
-    public void transform(Repository repository, URI graph, TransformContext context) throws TransformException {
-        try {
-            RepositoryConnection con = repository.getConnection();
-            try {
-                Update q = con.prepareUpdate(QueryLanguage.SPARQL, "DEFINE sql:log-enable 3 WITH <" + graph + "> DELETE {?vvvo ?vvvvp ?vvvvo} WHERE  {?s <" + ODSVoc.ODS_RAW_CATALOG + "> ?v. ?v ?vp ?vo. ?vo ?vvp ?vvo. ?vvo ?vvvp ?vvvo. ?vvvo ?vvvvp ?vvvvo}");
-                q.execute();
-                q = con.prepareUpdate(QueryLanguage.SPARQL, "DEFINE sql:log-enable 3 WITH <" + graph + "> DELETE {?vvo ?vvvp ?vvvo} WHERE  {?s  <" + ODSVoc.ODS_RAW_CATALOG + "> ?v. ?v ?vp ?vo. ?vo ?vvp ?vvo. ?vvo ?vvvp ?vvvo}");
-                q.execute();
-                q = con.prepareUpdate(QueryLanguage.SPARQL, "DEFINE sql:log-enable 3  WITH <" + graph + "> DELETE {?vo?vvp ?vvo} WHERE  {?s  <" + ODSVoc.ODS_RAW_CATALOG + "> ?rawCatalog. ?rawCatalog ?p ?o. ?o ?vp ?vo. ?vo ?vvp ?vvo}");
-                q.execute();
-                q = con.prepareUpdate(QueryLanguage.SPARQL, "DEFINE sql:log-enable 3  WITH <" + graph + "> DELETE {?o ?vp ?vo} WHERE  {?s  <" + ODSVoc.ODS_RAW_CATALOG + "> ?rawCatalog. ?rawCatalog ?p ?o. ?o ?vp ?vo}");
-                q.execute();
-                q = con.prepareUpdate(QueryLanguage.SPARQL, "DEFINE sql:log-enable 3  WITH <" + graph + "> DELETE {?rawCatalog ?p ?o} WHERE  {?s  <" + ODSVoc.ODS_RAW_CATALOG + "> ?rawCatalog. ?rawCatalog ?p ?o}");
-                q.execute();
-                con.commit();
-            } catch (RepositoryException e) {
-                throw new TransformException(e.getMessage(), e);
-            } finally {
-                con.close();
-            }
-        } catch (Exception e) {
-            throw new TransformException(e.getMessage(), e);
-        }
+  @Override
+  public void transform(Repository repository, URI graph, TransformContext context) throws TransformException {
+    try {
+      RepositoryConnection con = repository.getConnection();
+      try {
+        /*
+       Creative use of property paths to delete all triples starting in a node
+        */
+        String deleteQuery = "DEFINE sql:log-enable 3 " +
+                "WITH <" + graph + "> " +
+                "DELETE {?foo ?bang ?bar } " +
+                "WHERE {" +
+                "   SELECT distinct(?foo) ?bang ?bar" +
+                "   WHERE {" +
+                "     ?s <" + ODSVoc.ODS_RAW_CATALOG + "> ?raw." +
+                "     ?raw !<http://unexistingURI>* ?foo." +
+                "     ?foo ?bang ?bar. " +
+                "   }" +
+                "}";
+        Update q = con.prepareUpdate(QueryLanguage.SPARQL, deleteQuery);
+        q.execute();
+        con.commit();
+      } catch (RepositoryException e) {
+        throw new TransformException(e.getMessage(), e);
+      } finally {
+        con.close();
+      }
+    } catch (Exception e) {
+      throw new TransformException(e.getMessage(), e);
     }
+  }
 
-    @Override
-    public String getName() {
-        return "ODS Cleaner";
-    }
+  @Override
+  public String getName() {
+    return "ODS Cleaner";
+  }
 
-    @Override
-    public String getDescription() {
-        return "Cleans up any raw data present after harmonization. Only works if the virtuoso extractor is also part of the pipeline.";
-    }
+  @Override
+  public String getDescription() {
+    return "Cleans up any raw data present after harmonization. Only works if the virtuoso extractor is also part of the pipeline.";
+  }
 
-    @SuppressWarnings("ReturnOfNull")
-    @Override
-    public Resource getIcon(Application application) {
-        return new ClassResource("/ods.png", application);
-    }
+  @SuppressWarnings("ReturnOfNull")
+  @Override
+  public Resource getIcon(Application application) {
+    return new ClassResource("/ods.png", application);
+  }
 
-    @Override
-    public String asString() {
-        return getName();
-    }
+  @Override
+  public String asString() {
+    return getName();
+  }
 
-    @Override
-    protected void configureInternal(Object config) throws ConfigurationException {
-    }
+  @Override
+  protected void configureInternal(Object config) throws ConfigurationException {
+  }
 }
