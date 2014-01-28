ec91683ed5162da1163775e54c2101fba5a17aa0
nielsv <niels.vandekeybus@tenforce.com>
2014-01-28 01:51:44 +0100

    value mapper: auto trim harmonized value

--- lodms-ods-transformers/dcat-valuemapper/src/main/java/com/tenforce/lodms/transformers/Mapping.java
+++ lodms-ods-transformers/dcat-valuemapper/src/main/java/com/tenforce/lodms/transformers/Mapping.java
@@ -1,32 +1,34 @@
 package com.tenforce.lodms.transformers;
 
 public class Mapping {
-    private String originalValue;
-    private String harmonizedValue;
-    private boolean validMapping = true;
+  private String originalValue;
+  private String harmonizedValue;
+  private boolean validMapping = true;
 
-    public Mapping() {
-        this("","http://example.org/me#");
-    }
-    public Mapping(String originalValue,String harmonizedValue) {
-        this.originalValue = originalValue;
-        this.harmonizedValue = harmonizedValue;
-    }
+  public Mapping() {
+    this("", "http://example.org/me#");
+  }
 
-    public String getHarmonizedValue() {
-        return harmonizedValue;
-    }
-    public void setHarmonizedValue(String harmonizedValue) {
-        this.harmonizedValue = harmonizedValue;
-    }
+  public Mapping(String originalValue, String harmonizedValue) {
+    this.originalValue = originalValue;
+    this.harmonizedValue = harmonizedValue;
+  }
 
-    public String getOriginalValue() {
-        return originalValue;
-    }
+  public String getHarmonizedValue() {
+    return harmonizedValue;
+  }
 
-    public void setOriginalValue(String originalValue) {
-        this.originalValue = originalValue;
-    }
+  public void setHarmonizedValue(String harmonizedValue) {
+    this.harmonizedValue = harmonizedValue.trim();
+  }
+
+  public String getOriginalValue() {
+    return originalValue;
+  }
+
+  public void setOriginalValue(String originalValue) {
+    this.originalValue = originalValue;
+  }
 
   public boolean isValidMapping() {
     return validMapping;
